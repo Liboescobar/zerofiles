304b0b71a15332987f1178a6fc75466a12bd33c1
zero <94368175+zerosquad13@users.noreply.github.com>
2021-12-26 01:17:05 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("💥𝐂𝐫𝐞𝐚𝐭𝐨𝐫💥", url="t.me/fuckinzero13"),
-                    InlineKeyboardButton("💥𝐑𝐞𝐩𝐨💥", url="t.me/xproject13")
+                    InlineKeyboardButton("💥𝐂𝐫𝐞𝐚𝐭𝐨𝐫💥", url = "t.me/fuckinzero13"),
+                    InlineKeyboardButton("💥𝐑𝐞𝐩𝐨💥", url = "t.me/xproject13")
                 ],
                 [
                     InlineKeyboardButton("💥𝐂𝐥𝐨𝐬𝐞💥", callback_data = "close")
